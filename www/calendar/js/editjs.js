8b0d65a63520c176cf7c15954c8bf0a07dfbc4a6
kik <tachanok.r@gmail.com>
2017-06-08 15:14:52 +0700

    no message

---
+++ www/calendar/js/editjs.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
+    $(".dropdown-button").dropdown();
+    $('.chips').material_chip();
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+});
